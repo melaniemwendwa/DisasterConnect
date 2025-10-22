7876dd1b4a5d0ae754b60a81ca92ee204d0a59d9
mohammedhassa15 <mohammedhassan0041@gmail.com>
2025-10-22 12:06:29 +0300

    add pages files and routes file to the client

---
+++ client/src/routes/routes.jsx
@@ -0,0 +1,81 @@
+import App from "../App.jsx";
+import Home from "../pages/Home.jsx";
+import Login from "../pages/Login.jsx"; 
+import Dashboard from "../pages/Dashboard.jsx";
+import ReportForm from "../pages/ReportForm.jsx";
+import Signup from "../pages/Signup.jsx";
+import LandingPage from "../pages/Landingpage.jsx";
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../context/UserContext.jsx";
+import EditReport from "../pages/EditReport.jsx";
+import ReportDetail from "../pages/ReportDetail.jsx"
+
+
+const ProtectedRoute = ({ children }) => {
+  const { user } = useAuth();
+  if (!user) return <Navigate to="/" replace />;
+  return children;
+};
+
+const routes = [
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <LandingPage />,
+      },
+      {
+        path: "/home",
+        element: (
+          <ProtectedRoute>
+            <Home />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/dashboard",
+        element: (
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "/projects/:id",
+        element: (
+          <ProtectedRoute>
+            <ReportDetail />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "/ReportForm",
+        element: (
+          <ProtectedRoute>
+            <ReportForm />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "/signup",
+        element: <Signup />,
+      },
+      {
+        path: "/projects/:id/edit",
+        element:(
+          <ProtectedRoute>
+            <EditReport/>
+          </ProtectedRoute>
+        )
+      }
+    ]
+  }
+];
+
+export default routes;
